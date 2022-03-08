5cf0ec185cb3f5b7c087a16e1524e6f851af8499
gawd-lol <94883170+gawd-lol@users.noreply.github.com>
2022-03-08 04:58:12 +0000

    not in the simplest form

--- p1final.c
+++ p1final.c
@@ -1,1 +1,39 @@
 #include <stdio.h>
+#include <math.h>
+
+void input(int *num1, int *den1, int *num2, int *den2)
+{
+  printf("Enter the numerator and denominator of 1st fraction: \n");
+  scanf("%d %d", num1, den1);
+  printf("Enter the numerator and denominator of 2nd fraction: \n");
+  scanf("%d %d", num2, den2);
+}
+
+void add(int num1,int den1, int num2, int den2, int *num3, int *den3)
+{
+  *num3=((num1*den2)+(den1*num2));
+  *den3=(den1*den2);
+  int i, lcm=0;
+  for(i=1; i<=*num3 && i<=*den3; i++)
+    {
+      if(*num3%i==0 && *den3%i==0)
+      {
+        lcm=i;
+      }
+    }
+}
+
+
+void output(int num1, int den1, int num2, int den2, int num3, int den3)
+{
+  printf("The sum of fractions %d/%d and %d/%d is %d/%d", num1, den1, num2, den2, num3, den3);
+}
+
+int main()
+{
+  int num1, den1, num2, den2, num3, den3;
+  input(&num1, &den1, &num2, &den2);
+  add(num1, den1, num2, den2, &num3, &den3);
+  output(num1, den1, num2, den2, num3, den3);
+  return 0;
+}
--- p2final.c
+++ p2final.c
@@ -1,1 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <math.h>
+
+struct _fraction
+{
+int num, den;
+}
+
+typedef _fraction Fraction
+
+Fraction input_fraction()
+{
+  Fraction x;
+  printf("Enter the numerator and denominator of the fraction: \n");
+  scanf("%d %d", &x.num, &x.den);
+  return x;
+}
+
+Fraction Largest_fraction(Fraction f1, Fraction f2, Fraction f3)
+{}
+
+void output(Fraction f1, Fraction f2, Fraction f3, Fraction largest)
+{}
+
+int main()
+{}
